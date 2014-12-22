5971d5ef86bf804460020e36bf61d118e7141eb4
urkoalber <urkito91@hotmail.com>
2014-12-22 14:15:17 +0100

    first release

--- public/app.js
+++ public/app.js
@@ -1,22 +1,33 @@
 $( document ).ready( function(){
-	$('#go').click( function(e){
-		e.preventDefault();
-		$('#empty').hide();
-		$('#patents').show(); $('#academic').show(); $('#news').show();
-		var googleUrl = 'http://google.com/patents?output=rss%26q=';
-		var searchText = $('#search').val().replace(/ /g, '+');
-
-		var xhr = $.getJSON('/api/feed?url=' + googleUrl + searchText, function(data){
-			$('#patents').find('.loader').first().hide();
-			$('#patents').append('<a href=' + data.url + '>' + data.title + '</a>');
+	function loadData(url, searchText, destination) {
+		$.getJSON('/api/feed?url=' + url + searchText, function(data){
+			$(destination).find('.loader').first().hide();
+			$(destination).find('.data').append('<a href=' + data.url + '>' + data.title + '</a>');
 			var listData = '<ul>';
 			for(var i=0; i < data.items.length; i++){
 				var item = data.items[i];
 				listData += '<li><a href=' + item.url + '>' + item.title + '</a><p>' + item.summary + '</p></li>';
 			}
 			listData += '</ul>';
-			$('#patents').append(listData);
+			$(destination).find('.data').append(listData);
 		});
+	}
+	
+	$('#go').click( function(e){
+		e.preventDefault();
+		$('#empty').hide();
+		$('#patents').show(); $('#academic').show(); $('#news').show();		
+		var searchText = $('#search').val().replace(/ /g, '+');
+		
+		var googleUrl = 'http://google.com/patents?output=rss%26q=';		
+		loadData(googleUrl, searchText, '#patents');
+		
+		var microsoftUrl = 'http://academic.research.microsoft.com/Rss?end=15%26query=';
+		loadData(microsoftUrl, searchText, '#academic');
+		
+		var yahooUrl = 'http://es.news.search.yahoo.com/rss?p=';
+		loadData(yahooUrl, searchText, '#news');
+		
 		return false;
 	});
 });
