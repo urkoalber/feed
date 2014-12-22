e65f5aa671f7a2b404596d980a57e1ba372201b7
albaoe <gectorus@gmail.com>
2014-12-22 14:01:40 +0100

    food2

--- public/app.js
+++ public/app.js
@@ -9,6 +9,13 @@ $( document ).ready( function(){
 		var xhr = $.getJSON('/api/feed?url=' + googleUrl + searchText, function(data){
 			$('#patents').find('.loader').first().hide();
 			$('#patents').append('<a href=' + data.url + '>' + data.title + '</a>');
+			var listData = '<ul>';
+			for(var i=0; i < data.items.length; i++){
+				var item = data.items[i];
+				listData += '<li><a href=' + item.url + '>' + item.title + '</a><p>' + item.summary + '</p></li>';
+			}
+			listData += '</ul>';
+			$('#patents').append(listData);
 		});
 		return false;
 	});
